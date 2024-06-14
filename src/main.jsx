6262a178da0a576c786ed69f6a8021d00534a158
Sunil <sv784333@gmial.com>
2024-06-14 08:32:22 +0530

    bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,5 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
-    document.getElementById('root')
 )
